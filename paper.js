71e896c35bac3fb3b03d965063956e98614e87aa
CVDMR <167243523+CVDMR@users.noreply.github.com>
2026-04-25 13:01:58 -0300

    Get logic getComputerChoice function

---
+++ paper.js
@@ -0,0 +1,24 @@
+
+
+console.log("asf")
+// alert("jal")
+
+function getComputerChoice() {
+
+    let number =  Math.random();
+
+    let choice;
+
+
+    if (number < 0.3) {
+        choice = "paper";
+    } else if (number >= 0.3 && number < 0.6) {
+        choice = "rock";
+    } else {
+        choice = "scissors";
+    }
+    
+    return choice
+}
+
+console.log(getComputerChoice())
